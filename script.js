db48ecdb2940c12800908796c11d3a9e15f1e35f
Secrechrono <xrandreadis2006@gmail.com>
2026-05-10 21:11:46 +0300

    Risky commit

--- script.js
+++ script.js
@@ -352,3 +352,16 @@ if ('ontouchstart' in window) {
     }
   })
 }
+
+fetch("https://hits.sh/secrechrono.github.io/Lore.svg")
+  .then(r => r.text())
+  .then(svg => {
+    const match = svg.match(/>(\d+)<\/text>/g)
+    if (match) {
+      const count = match[match.length - 1].replace(/<\/?[^>]+(>|$)/g, "")
+      document.querySelector(".visitor-count").textContent = count + " visitors"
+    }
+  })
+  .catch(() => {
+    document.querySelector(".visitor-count").textContent = "Visitor count unavailable :("
+  })
